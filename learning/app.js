8323f480704708c0f3db665827f899db6846e8bf
rninsung <kms73033222@gmail.com>
2021-09-15 23:46:55 +0900

    커밋
    커밋

--- learning/app.js
+++ learning/app.js
@@ -2,11 +2,8 @@ const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
     const clickedClass = "clicked";
-    if (h1.classList.contains(clickedClass)){
-        h1.classList.remove(clickedClass);
-    } else{
-        h1.classList.add(clickedClass);
-    }
+    h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleTitleClick);
+// toggle을 활용한 간단한 클릭 이벤트
