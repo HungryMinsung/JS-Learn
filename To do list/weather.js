0aa9e6a09216b123adb39af4844633d7d1cde079
rninsung <kms73033222@gmail.com>
2021-09-22 23:23:28 +0900

    weather api
    weather api를 사용해서 현재 나의 위치를 표시해보자.

--- To do list/weather.js
+++ To do list/weather.js
@@ -1,7 +1,12 @@
+const API_KEY = "f8340bb0add5e0750d7e46f32f4779e9"
+
+
+
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
     console.log("You live in",lat, lng);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=f8340bb0add5e0750d7e46f32f4779e9=${API_KEY}`
 }
 function onGeoError(){
     alert("Can't find yo. No weather for you.");
