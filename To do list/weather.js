7afd411027ae4e0edc45f9d9d1f4775d4e0db59b
rninsung <kms73033222@gmail.com>
2021-09-23 23:18:08 +0900

    Todolist(7) weather api
    내 지역의 위치와 현재 도시와 날씨를 표시해보자.

--- To do list/weather.js
+++ To do list/weather.js
@@ -4,9 +4,12 @@ const API_KEY = "f8340bb0add5e0750d7e46f32f4779e9"
 
 function onGeoOk(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in",lat, lng);
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=f8340bb0add5e0750d7e46f32f4779e9=${API_KEY}`
+    const lon = position.coords.longitude;
+    console.log(lat, lon);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    fetch(url).then(response => response.json()).then(data => {
+        console.log(data.name);
+    });
 }
 function onGeoError(){
     alert("Can't find yo. No weather for you.");
